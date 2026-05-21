ce62ad8829ce1e0eb285603c153dfaf04b302de4
portalcrafterdev <185675664+portalcrafterdev@users.noreply.github.com>
2026-05-21 19:16:01 +0530

    introduce ProfileScreen

---
+++ src/screens/ProfileScreen.tsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import { StyleSheet, ScrollView, Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+import ProfileHeader from '../components/profile_components/ProfileHeader';
+import ProfileInfo from '../components/profile_components/ProfileInfo';
+import ProfileButtons from '../components/profile_components/ProfileButtons';
+import StoryHighlight from '../components/profile_components/StoryHighlight';
+import ProfileTabs from '../components/profile_components/ProfileTabs';
+import ProfileGrid from '../components/profile_components/ProfileGrid';
+
+
+const ProfileScreen = () => {
+  const [activeTab, setActiveTab] = useState('posts');
+
+  return (
+    <SafeAreaView style={styles.container} edges={['top']}>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
