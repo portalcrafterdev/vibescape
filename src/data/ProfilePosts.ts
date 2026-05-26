dbea2240d30ba0fe6527a363f72342c2914ee420
portalcrafterdev <185675664+portalcrafterdev@users.noreply.github.com>
2026-05-26 17:19:32 +0530

    add ProfilePosts

---
+++ src/data/ProfilePosts.ts
@@ -0,0 +1,21 @@
+export const profilePosts = [
+  {
+    id: '1',
+    image: 'https://picsum.photos/id/1011/600/750',
+    pinned: true,
+  },
+  {
+    id: '2',
+    image: 'https://picsum.photos/id/1012/600/750',
+    pinned: true,
+  },
+  {
+    id: '3',
+    image: 'https://picsum.photos/id/1013/600/750',
+    pinned: true,
+  },
+  {
+    id: '4',
+    image: 'https://picsum.photos/id/1014/600/750',
+  },
+  {
