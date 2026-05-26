8f51ec87bf8f0b2f87b879d9ce88e6d4c2d39067
portalcrafterdev <185675664+portalcrafterdev@users.noreply.github.com>
2026-05-26 18:53:08 +0530

    scaffold Reels

---
+++ src/data/Reels.ts
@@ -0,0 +1,23 @@
+export const reels = [
+  {
+    id: '1',
+    username: 'hardik',
+    profile: 'https://i.pravatar.cc/150?img=1',
+    video: {
+      uri: 'https://www.w3schools.com/html/mov_bbb.mp4',
+    },
+    album: 'https://i.pravatar.cc/150?img=11',
+    caption: 'Enjoying React Native 🚀',
+    likes: '12.5K',
+    comments: '420',
+  },
+  {
+    id: '2',
+    username: 'developer',
+    profile: 'https://i.pravatar.cc/150?img=2',
+    video: {
+  uri: 'https://samplelib.com/lib/preview/mp4/sample-15s.mp4'
+},
+
+    album: 'https://i.pravatar.cc/150?img=12',
+    caption: 'Coding all night 💻',
