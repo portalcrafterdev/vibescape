17cce608c8643c732b6b068d928465c9da9db1fd
portalcrafterdev <185675664+portalcrafterdev@users.noreply.github.com>
2026-06-03 19:31:21 +0530

    set up EditProfileHeader

---
+++ src/components/Edit_Profile/EditProfileHeader..tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import {
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+} from "react-native";
+
+import { ArrowLeft } from "lucide-react-native";
+import { useNavigation } from "@react-navigation/native";
+
+const EditProfileHeader = () => {
+  const navigation = useNavigation<any>();
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity
+        style={styles.backButton}
+        onPress={() => navigation.goBack()}
+      >
+        <ArrowLeft
+          size={26}
