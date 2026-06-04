f3b185315b4d5df5da6766fb782a59a98354771f
portalcrafterdev <185675664+portalcrafterdev@users.noreply.github.com>
2026-06-04 19:41:45 +0530

    refine PostCard

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -20,4 +20,27 @@ interface Props {
 }
 
 const PostCard = ({ item }: Props) => {
-  const [liked, setLiked] = useState(false);
+  const [liked, setLiked] = useState(false);
+
+  return (
+    <View style={styles.container}>
+
+      {/* Header */}
+      <View style={styles.header}>
+        <View style={styles.userRow}>
+          <Image
+            source={{ uri: item.profileImage }}
+            style={styles.avatar}
+          />
+
+          <Text style={styles.username}>
+            {item.username}
+          </Text>
+        </View>
+
+        <EllipsisVertical color="white" size={20} />
+      </View>
+
+      {/* Post Image */}
+      <Image
+        source={{ uri: item.postImage }}
