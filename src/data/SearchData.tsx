3ca5a0134c78280fcad5f16b8a31ae89689c86af
portalcrafterdev <185675664+portalcrafterdev@users.noreply.github.com>
2026-06-11 18:00:53 +0530

    create SearchData

---
+++ src/data/SearchData.tsx
@@ -0,0 +1,25 @@
+export const searchData = [
+  {
+    id: '1',
+    image: 'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?w=800',
+    views: '820K',
+    isVideo: true,
+  },
+  {
+    id: '2',
+    image: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?w=800',
+    views: '293K',
+    isVideo: true,
+  },
+  {
+    id: '3',
+    image: 'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?w=800',
+    views: '217K',
+    isVideo: true,
+  },
+  {
+    id: '4',
+    image: 'https://images.unsplash.com/photo-1517841905240-472988babdf9?w=800',
+    views: '2.7M',
+    isVideo: true,
+  },
