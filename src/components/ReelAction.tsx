9b9c651e59dc79c2335cdaa6e390e8ac7f6ea053
portalcrafterdev <185675664+portalcrafterdev@users.noreply.github.com>
2026-06-18 19:39:48 +0530

    set up ReelAction

---
+++ src/components/ReelAction.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+} from 'react-native';
+
+import {
+  Heart,
+  MessageCircle,
+  Repeat2,
+  Send,
+  Bookmark,
+  MoreHorizontal,
+} from 'lucide-react-native';
+
+const ReelActions = ({ item }: any) => {
+  return (
