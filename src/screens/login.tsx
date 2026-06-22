79bb5e7c9de65efa5273d09bd76f16a53c7cb79e
portalcrafterdev <185675664+portalcrafterdev@users.noreply.github.com>
2026-06-22 17:05:23 +0530

    update login

--- src/screens/login.tsx
+++ src/screens/login.tsx
@@ -50,4 +50,58 @@ const LoginScreen = ({ navigation }: Props) => {
   >  
    <View style={styles.logocontainer}>
 
-    <Image source={require('../assets/images/clipart402911.png')}  style={styles.logoImage}
+    <Image source={require('../assets/images/clipart402911.png')}  style={styles.logoImage}
+    resizeMode="contain">
+
+    </Image>
+    <Text style={styles.logo}> Instagram</Text>
+  </View>
+
+      <TextInput
+        placeholder="Email or Username"
+        placeholderTextColor="#888"
+        value={email}
+        onChangeText={setEmail}
+        style={styles.input}
+      />
+
+      <View style={styles.passwordContainer}>
+        <TextInput
+          placeholder="Password"
+          placeholderTextColor="#888"
+          secureTextEntry={hidePassword}
+          value={password}
+          onChangeText={setPassword}
+          style={styles.passwordInput}
+        />
+
+        <TouchableOpacity
+          onPress={() => setHidePassword(!hidePassword)}
+        >
+          {hidePassword ? (
+            <EyeOff color="white" size={22} />
+          ) : (
+            <Eye color="white" size={22} />
+          )}
+        </TouchableOpacity>
+      </View>
+
+      <TouchableOpacity
+        onPress={() => navigation.navigate('Forgot')}
+      >
+        <Text style={styles.forgot}>
+          Forgot Password?
+        </Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={styles.loginButton}
+        onPress={()=> navigation.navigate('Maintabs')}
+      >
+        <Text style={styles.loginText}>
+          Log In
+        </Text>
+      </TouchableOpacity>
+
+<View style={{flexDirection: "row"}}>
+
