45c7e7c3b9b7be2928f162dd32868a6f5ded15c3
portalcrafterdev <185675664+portalcrafterdev@users.noreply.github.com>
2026-06-23 17:12:33 +0530

    update ReelAction

--- src/components/ReelAction.tsx
+++ src/components/ReelAction.tsx
@@ -17,4 +17,24 @@ import {
 } from 'lucide-react-native';
 
 const ReelActions = ({ item }: any) => {
-  return (
+  return (
+    <View style={styles.container}>
+
+   <TouchableOpacity style={styles.item}>
+  <Heart color="white" size={23} strokeWidth={2} />
+  <Text style={styles.count}>Likes</Text>
+</TouchableOpacity>
+
+<TouchableOpacity style={styles.item}>
+  <MessageCircle color="white" size={23} strokeWidth={2} />
+  <Text style={styles.count}>784</Text>
+</TouchableOpacity>
+
+<TouchableOpacity style={styles.item}>
+  <Repeat2 color="white" size={23} strokeWidth={2} />
+  <Text style={styles.count}>1,370</Text>
+</TouchableOpacity>
+
+<TouchableOpacity style={styles.item}>
+  <Send color="white" size={23} strokeWidth={2} />
+</TouchableOpacity>
