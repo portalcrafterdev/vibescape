d068ada8190f2685c383ff9efb9f800cfb39df14
portalcrafterdev <185675664+portalcrafterdev@users.noreply.github.com>
2026-06-25 17:06:16 +0530

    add customTabBar

---
+++ src/component/customTabBar.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  View,
+  TouchableOpacity,
+  StyleSheet,
+} from 'react-native';
+
+import {
+  BottomTabBarProps,
+} from '@react-navigation/bottom-tabs';
+
+import {
+  House,
+  Search,
+  SquarePlus,
+  Clapperboard,
+  CircleUserRound,
+  Send,
+} from 'lucide-react-native';
+
+const icons = {
