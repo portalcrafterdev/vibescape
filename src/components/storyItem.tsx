4fee804c25d33e6c19a722588a676c73bd486c8b
portalcrafterdev <185675664+portalcrafterdev@users.noreply.github.com>
2026-06-30 17:34:23 +0530

    extend storyItem

--- src/components/storyItem.tsx
+++ src/components/storyItem.tsx
@@ -39,4 +39,24 @@ export default StoryItem;
 const styles = StyleSheet.create({
   container: {
     width: 80,
-    alignItems: 'center',
+    alignItems: 'center',
+    marginRight: 10,
+  },
+
+  storyBorder: {
+    width: 76,
+    height: 76,
+    borderRadius: 38,
+    borderWidth: 3,
+    borderColor: '#ff3040',
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'relative',
+  },
+
+  image: {
+    width: 68,
+    height: 68,
+    borderRadius: 34,
+  },
+
