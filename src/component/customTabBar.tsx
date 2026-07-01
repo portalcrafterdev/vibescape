41c01c430a0c5f1be4e233d508c52530d3d80d3c
portalcrafterdev <185675664+portalcrafterdev@users.noreply.github.com>
2026-07-01 17:18:25 +0530

    update customTabBar

--- src/component/customTabBar.tsx
+++ src/component/customTabBar.tsx
@@ -18,4 +18,25 @@ import {
   Send,
 } from 'lucide-react-native';
 
-const icons = {
+const icons = {
+  Home: House,
+  Search: Search,
+  Create: Send,
+  Reels: Clapperboard,
+  Profile: CircleUserRound,
+};
+
+export default function CustomTabBar({
+  state,
+  descriptors,
+  navigation,
+}: BottomTabBarProps) {
+  return (
+    <View style={styles.wrapper}>
+      <View style={styles.container}>
+        {state.routes.map((route, index) => {
+          const focused = state.index === index;
+
+          const Icon =
+            icons[route.name as keyof typeof icons];
+
