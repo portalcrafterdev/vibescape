c4041bc2aea881d813b46a302a2ada91bee042db
portalcrafterdev <185675664+portalcrafterdev@users.noreply.github.com>
2026-07-03 17:26:36 +0530

    update ProfileButtons

--- src/components/profile_components/ProfileButtons.tsx
+++ src/components/profile_components/ProfileButtons.tsx
@@ -21,4 +21,28 @@ const ProfileButtons =()=>{
     style={styles.addButton}
     accessibilityRole="button"
     accessibilityLabel="Discover people">
-    <UserPlus
+    <UserPlus
+    color={"white"}
+    size={20}
+    strokeWidth={2}/>
+  </TouchableOpacity>
+
+  </View>
+    );
+};
+
+export default ProfileButtons;
+
+const styles = StyleSheet.create({
+
+    container: {
+    flexDirection: "row",
+    paddingHorizontal: 16,
+    alignItems: "center",
+    gap: 8,
+    marginTop: 18,
+    marginBottom: 18
+    },
+
+    button:{
+        flex: 1,
