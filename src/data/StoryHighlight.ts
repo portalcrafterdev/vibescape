4e0cfdb776f9532d18ed18c1b5aa5260fc545f31
portalcrafterdev <185675664+portalcrafterdev@users.noreply.github.com>
2026-07-06 17:26:06 +0530

    extend StoryHighlight

--- src/data/StoryHighlight.ts
+++ src/data/StoryHighlight.ts
@@ -17,4 +17,24 @@ export const highlights: Highlight[] = [
     image: 'https://dummyimage.com/200x200/000/ffffff&text=P',
   },
   {
-    id: '2',
+    id: '2',
+    title: 'O',
+    image: 'https://dummyimage.com/200x200/000/ffffff&text=O',
+  },
+  {
+    id: '3',
+    title: 'R',
+    image: 'https://dummyimage.com/200x200/000/ffffff&text=R',
+  },
+  {
+    id: '4',
+    title: 'T',
+    image: 'https://dummyimage.com/200x200/000/ffffff&text=T',
+  },
+  {
+    id: '5',
+    title: 'A',
+    image: 'https://dummyimage.com/200x200/000/ffffff&text=A',
+  },
+  {
+    id: '6',
