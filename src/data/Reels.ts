faed975f0867d5c2368179ee9d9aae3a1afb043e
portalcrafterdev <185675664+portalcrafterdev@users.noreply.github.com>
2026-07-22 18:21:37 +0530

    complete Reels

--- src/data/Reels.ts
+++ src/data/Reels.ts
@@ -65,4 +65,26 @@ export const reels = [
 },
 
     album: 'https://i.pravatar.cc/150?img=12',
-    caption: 'Coding all night 💻',
+    caption: 'Coding all night 💻',
+    likes: '8.3K',
+    comments: '190',
+  },
+
+{
+      id: '6',
+    username: 'developer',
+    profile: 'https://i.pravatar.cc/150?img=2',
+    video: {
+  uri: 'https://filesamples.com/samples/video/mp4/sample_960x540.mp4'
+},
+
+    album: 'https://i.pravatar.cc/150?img=12',
+    caption: 'Coding all night 💻',
+    likes: '8.3K',
+    comments: '190',
+  },
+
+
+
+  
+];
