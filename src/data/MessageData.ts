8cda05acc6738acc7fb643a26d5cc90c5eca961c
portalcrafterdev <185675664+portalcrafterdev@users.noreply.github.com>
2026-07-29 13:54:25 +0530

    feat: implement MessageItem and MessageList components; update CreateScreen to include MessageList

---
+++ src/data/MessageData.ts
@@ -0,0 +1,51 @@
+export const MessageData =[
+    {
+        id: "1",
+        username:"Sahil",
+        message:"sahil reacted to....",
+        time: "now",
+        unread: true,
+        online: false,
+        image: "https://randomuser.me/api/portraits/men/1.jpg",
+    },
+
+    {
+        id: "2",
+        username: "Akash",
+        message: "Sent",
+        time: "2h ago",
+        unread: false,
+        online: false,
+        image: "https://randomuser.me/api/portraits/men/2.jpg"
+    },
+
+    {
+        id: "3",
+        username: "Sahil Patyal",
+        message: "Sent",
+        time: "3h ago",
+        unread: false,
+        online : true,
+        image: "https://randomuser.me/api/portraits/men/3.jpg"
+    },
+
+     {
+        id: "4",
+        username: "Kulbir",
+        message: "Sent",
+        time: "3h ago",
+        unread: true,
+        online : true,
+        image: "https://randomuser.me/api/portraits/men/4.jpg"
+    },
+
+     {
+        id: "5",
+        username: "Shreya",
+        message: "Sent",
+        time: "now",
+        unread: false,
+        online : true,
+        image: "https://randomuser.me/api/portraits/men/5.jpg"
+    },
+]
