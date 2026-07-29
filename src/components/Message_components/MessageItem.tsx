22e6eafc30e4bcbb74914aba6d8595c08aff266f
portalcrafterdev <185675664+portalcrafterdev@users.noreply.github.com>
2026-07-29 14:04:13 +0530

    fix: format import statements in MessageItem component

--- src/components/Message_components/MessageItem.tsx
+++ src/components/Message_components/MessageItem.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View,Text,Image,TouchableOpacity, StyleSheet,} from "react-native";
+import {View,Text,Image,TouchableOpacity, StyleSheet,} from "react-native";
 
 const MessageItem = ({ item }: any) => {
   return (
