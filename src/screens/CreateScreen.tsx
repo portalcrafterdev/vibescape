dbf8580342668b76d25230aef9877c598de1ce84
portalcrafterdev <185675664+portalcrafterdev@users.noreply.github.com>
2026-07-29 11:56:44 +0530

    feat: add MessageSearchBar component to CreateScreen

--- src/screens/CreateScreen.tsx
+++ src/screens/CreateScreen.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { View, StyleSheet} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import MessageHeader from "../components/Message_components/MessageHeader";
+import MessageSearchBar from "../components/Message_components/MessageSearchBar";
 
 const CreateScreen = ()=>{
     return(
 <SafeAreaView style= {styles.container}>
     <View>
         <MessageHeader/>
+        <MessageSearchBar/>
     </View>
 </SafeAreaView>
     )
