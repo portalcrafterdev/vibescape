6eb556c095fdde72de4e5a017a4c6bffe76cdd40
portalcrafterdev <185675664+portalcrafterdev@users.noreply.github.com>
2026-07-29 19:39:42 +0530

    feat: enhance GenderScreen with selection options and integrate CircleDotIcon

--- src/components/Edit_Profile/GenderScreen.tsx
+++ src/components/Edit_Profile/GenderScreen.tsx
@@ -1,9 +1,10 @@
-import { Check, Cross, Plus, X } from "lucide-react-native";
-import React from "react";
+import { Check, Cross, Plus, X, Circle, CircleDot } from "lucide-react-native";
+import React, { useState } from "react";
 import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-
+import CircleDotIcon from "../icons/CircleDot";
 const GenderScreen = ()=>{
+    const [selected, setSelected] = useState("Male");
     return(
      <SafeAreaView style={styles.container}>
    <View>
@@ -21,6 +22,41 @@ const GenderScreen = ()=>{
     </View>
 
     <Text style={styles.subtitle}>This won't be the part of your public profile.</Text>
+
+    <View style= {styles.row}>
+      <Text style={styles.txt}>Female</Text>
+      {/* <Circle 
+      size={23}
+      color={"#fff"}
+      /> */}
+      <TouchableOpacity onPress={() => setSelected("Female")}>
+      <CircleDotIcon isSelected={selected == "Female"} height={23} width={23}  color={"#fff"}/>
+      </TouchableOpacity>
+    </View>
+
+        <View style= {styles.row}>
+      <Text style={styles.txt}>Male</Text>
+       
+      <TouchableOpacity onPress={() => setSelected("Male")}>
+      <CircleDotIcon isSelected={selected == "Male"} height={23} width={23}  color={"#fff"} />
+      </TouchableOpacity>
+    </View>
+
+        <View style= {styles.row}>
+      <Text style={styles.txt}>Custom</Text>
+      
+      <TouchableOpacity onPress={() => setSelected("custom")}>
+      <CircleDotIcon isSelected={selected == "custom"} height={23} width={23}  color={"#fff"} />
+      </TouchableOpacity>
+    </View>
+
+        <View style= {styles.row}>
+      <Text style={styles.txt}>Prefer not to say</Text>
+       
+      <TouchableOpacity onPress={() => setSelected("prefer")}>
+      <CircleDotIcon isSelected={selected == "prefer"} height={23} width={23}  color={"#fff"} />
+      </TouchableOpacity>
+    </View>
    </View>
    </SafeAreaView>   
     );
@@ -54,5 +90,19 @@ const styles = StyleSheet.create({
         color: "#a29e9e",
         marginHorizontal: 23,
         fontSize: 14
+    },
+
+    row: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        marginHorizontal:20,
+        marginVertical: 15,
+        marginTop: 30,
+    },
+
+    txt: {
+        color: "#fff",
+        fontSize: 16,
+        fontWeight: "700"
     }
 });
--- src/components/icons/CircleDot.tsx
+++ src/components/icons/CircleDot.tsx
@@ -1,23 +1,29 @@
 import * as React from "react"
-import Svg, { Path, SvgProps } from "react-native-svg"
+import Svg, { Circle, Path, SvgProps } from "react-native-svg"
 // import * as React from "react"
 // import Svg, { Path } from "react-native-svg"
 
-function CircleDotIcon(props:SvgProps) {
+interface CircleDotIconProps extends SvgProps {
+  isSelected?: boolean
+}
+
+function CircleDotIcon({ isSelected = false, ...props }: CircleDotIconProps) {
   return (
-    <Svg
-      width={props.width}
+     <Svg
+    width={props.width}
       height={props.height}
-      viewBox="0 0 24 24"
-      fill="#000"
-      // xmlns="http://www.w3.org/2000/svg"
-      {...props}
-    >
-      <Path
-            d="M213.333 0c117.821 0 213.334 95.513 213.334 213.333 0 117.821-95.513 213.334-213.334 213.334C95.513 426.667 0 331.154 0 213.333 0 95.513 95.513 0 213.333 0m0 42.667c-94.256 0-170.666 76.41-170.666 170.666C42.667 307.59 119.077 384 213.333 384 307.59 384 384 307.59 384 213.333c0-94.256-76.41-170.666-170.667-170.666m0 64c58.91 0 106.667 47.756 106.667 106.666S272.244 320 213.333 320c-58.91 0-106.666-47.756-106.666-106.667 0-58.91 47.756-106.666 106.666-106.666"
-        fill={props.color}
-      />
-    </Svg>
+    viewBox="0 0 512 512"
+    {...props}
+  >
+    <Path
+      d="M448,256c0-106-86-192-192-192S64,150,64,256s86,192,192,192S448,362,448,256Z"
+        fill="#fff"
+        stroke="#fff"
+        strokeMiterlimit={10}
+        strokeWidth={32}
+    />
+    {!isSelected && <Circle cx={256} cy={256} r={144} />}
+  </Svg>
   )
 }
 
